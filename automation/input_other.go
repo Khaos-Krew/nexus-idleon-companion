26cd72134b4cdf75ba8b368adb71a4cca58908e6
Khaos_Kirito <dasak93@gmail.com>
2026-08-25 23:58:33 -0500

    feat(automation): add non-Windows build stub

---
+++ automation/input_other.go
@@ -0,0 +1,14 @@
+//go:build !windows
+
+package main
+
+import "errors"
+
+type unsupportedInput struct{}
+
+func NewInputDriver() InputDriver { return unsupportedInput{} }
+func (unsupportedInput) FocusWindow(string) error { return errors.New("live automation requires Windows") }
+func (unsupportedInput) Click(int, int, bool) error { return errors.New("live automation requires Windows") }
+func (unsupportedInput) PressKey(string) error { return errors.New("live automation requires Windows") }
+func (unsupportedInput) KeyDown(string) bool { return false }
+func (unsupportedInput) CursorPosition() (Point, error) { return Point{}, errors.New("live automation requires Windows") }
